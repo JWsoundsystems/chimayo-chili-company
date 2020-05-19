eba1c3376fb555b988ab4db01ccbcfb1d4fa3020
Jeffrey Walsh <marwal@Jeffreys-iMac-Pro-4.local>
2020-05-19 09:33:14 -0700

    updated product and color scheme

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -13,13 +13,13 @@ const Layout = ({ children, site }) => (
         <div className="Wrap" >
           <div className="Header__body">
             <h1 className="Header__title">
-              <Link data-text={site.siteMetadata.siteName} to="/">
-                {site.siteMetadata.siteName}
+              <Link to="/">
+                Chimayo Chili Company
               </Link>
             </h1>
             <div className="Header__summary snipcart-summary snipcart-checkout">
               <div className="Header__summary__title">
-                🛍 MY CART 🛍
+                🛍 Chili Cart 🛍
               </div>
               <div className="Header__summary__line">
                 Number of items: <span className="snipcart-total-items"></span>
@@ -36,8 +36,9 @@ const Layout = ({ children, site }) => (
       </div>
       <div className="Wrap" >
         <div className="Footer">
-          This website is just an example project to demonstrate how you can 
-          integrate <a href="https://www.gatsbyjs.org/">Gatsby</a>, <a href="https://snipcart.com/">Snipcart</a> and <a href="https://www.datocms.com">DatoCMS</a>.
+          Chimayo Chili Company
+          <br></br>
+          Santa Fe, New Mexico <a href="mailto: chimayochilicompany@gmail.com">Contact Us</a>.
         </div>
       </div>
     </div>
